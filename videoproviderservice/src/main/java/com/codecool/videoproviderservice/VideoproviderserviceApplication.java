1b1f1f002c379e7a505be3897348bdc74c2fd000
Barnuss09 <hollbarna@gmail.com>
2020-08-07 15:24:04 +0200

    VideoRecommendationServiceApplication init

--- videoproviderservice/src/main/java/com/codecool/videoproviderservice/VideoproviderserviceApplication.java
+++ videoproviderservice/src/main/java/com/codecool/videoproviderservice/VideoproviderserviceApplication.java
@@ -2,8 +2,10 @@ package com.codecool.videoproviderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VideoproviderserviceApplication {
 
 	public static void main(String[] args) {
--- videoproviderservice/src/main/java/com/codecool/videoproviderservice/controller/VideoController.java
+++ videoproviderservice/src/main/java/com/codecool/videoproviderservice/controller/VideoController.java
@@ -3,6 +3,8 @@ package com.codecool.videoproviderservice.controller;
 
 import com.codecool.videoproviderservice.model.Video;
 import com.codecool.videoproviderservice.repository.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,9 @@ public class VideoController {
 
     private final VideoRepository videoRepository;
 
+    @Autowired
+    private Environment environment;
+
     public VideoController(VideoRepository videoRepository) {
         this.videoRepository = videoRepository;
     }
@@ -22,6 +27,11 @@ public class VideoController {
     @GetMapping("/all")
     public List<Video> getAll(){
         return videoRepository.findAll();
+    }
+
+    @GetMapping("/port")
+    public String port(){
+        return environment.getProperty("server.port");
     }
 
 }
