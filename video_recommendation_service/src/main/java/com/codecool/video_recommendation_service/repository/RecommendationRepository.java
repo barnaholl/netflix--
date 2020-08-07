3d501e6961659c30aaa574e532069ec2060bb8bb
Barnuss09 <hollbarna@gmail.com>
2020-08-07 17:50:06 +0200

    RecommendationCreator

--- video_recommendation_service/src/main/java/com/codecool/video_recommendation_service/repository/RecommendationRepository.java
+++ video_recommendation_service/src/main/java/com/codecool/video_recommendation_service/repository/RecommendationRepository.java
@@ -4,4 +4,5 @@ import com.codecool.video_recommendation_service.model.Recommendation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecommendationRepository extends JpaRepository<Recommendation,Long> {
+    Recommendation getByVideoId(Long id);
 }
