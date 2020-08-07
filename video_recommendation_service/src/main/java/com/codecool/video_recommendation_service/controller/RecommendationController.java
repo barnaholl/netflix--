4cc114e6415550d20e00d697f3e3a5e6ebf38014
Barnuss09 <hollbarna@gmail.com>
2020-08-07 17:50:22 +0200

    RecommendationController

---
+++ video_recommendation_service/src/main/java/com/codecool/video_recommendation_service/controller/RecommendationController.java
@@ -0,0 +1,21 @@
+package com.codecool.video_recommendation_service.controller;
+
+import com.codecool.video_recommendation_service.model.Recommendation;
+import com.codecool.video_recommendation_service.repository.RecommendationRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/recommendation")
+public class RecommendationController {
+
+    private final RecommendationRepository recommendationRepository;
+
+    public RecommendationController(RecommendationRepository recommendationRepository) {
+        this.recommendationRepository = recommendationRepository;
+    }
+
+    @GetMapping("/{id}")
+    public Recommendation getRecommendationById(@PathVariable("id") Long id){
+        return recommendationRepository.getByVideoId(id);
+    }
+}
