2de37f54b9e6135dcac77a797cb6fc671790a747
Barnuss09 <hollbarna@gmail.com>
2020-08-07 15:40:16 +0200

    RecommendationRepository

---
+++ video_recommendation_service/src/main/java/com/codecool/video_recommendation_service/repository/RecommendationRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.video_recommendation_service.repository;
+
+import com.codecool.video_recommendation_service.model.Recommendation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecommendationRepository extends JpaRepository<Recommendation,Long> {
+}
